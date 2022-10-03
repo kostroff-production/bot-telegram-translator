20b1408cb6f2f27b7b378f41839fa497940c0539
Jony <44669971+kostroff-production@users.noreply.github.com>
2022-10-03 17:37:25 +0300

    Update bot.py

--- Бот переводчик телеграмм/bot.py
+++ Бот переводчик телеграмм/bot.py
@@ -1,120 +1,123 @@
 import requests
-from keys import token, yandex, key, menu
-from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ParseMode
+import json
+from keys import token, yandex, IAM_TOKEN, folder_id, menu
+from telegram import Bot, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ParseMode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 
 # Функция обработчик команд
-def command(bot, update):
-	bot.send_message(
-		chat_id = update.message.chat_id,
-		text = 'Бот активирован 🤖\n Будут вопросы жми 🆘🤯⁉️',
-		reply_markup = keyboard_help()
-		)
+def command(update, bot):
+	update.message.reply_text(
+		text='Бот активирован 🤖\n Будут вопросы жми /info',
+		reply_markup=keyboard_help()
+	)
 
-def help_command(bot, update):
-	bot.send_message(
-		chat_id = update.message.chat_id,
-		text = menu
-		)
+def help_command(update, bot):
+	update.message.reply_text(text=menu)
 
 # Функция обработчки текста
-def txt(bot, update):
+def txt(update, bot):
 	text = update.message.text
-	bot.send_message(
-		chat_id = update.message.chat_id,
-		text = text,
-		reply_markup = lang()
-		)
+	update.message.reply_text(
+		text=text,
+		reply_markup=lang()
+	)
 
 # Кнопка помощи, которая будет активна постоянная и находиться под полем ввода текста
 def keyboard_help():
-	keyboard = [[KeyboardButton('/🆘🤯⁉️')]]
+	keyboard = [[KeyboardButton('/info')]]
 	return ReplyKeyboardMarkup(
-		keyboard = keyboard,
-		resize_keyboard = True
+		keyboard=keyboard,
+		resize_keyboard=True
 		)
 
 # Инлайн кнопки, кнопки работающие с текстом, принимающие запрос от пользователя на перевод
 # первый ряд
 def lang():
-	keyboard = [[InlineKeyboardButton('🏴󠁧󠁢󠁥󠁮󠁧󠁿', callback_data = 'ru-en'),
-				InlineKeyboardButton('🇩🇪', callback_data = 'ru-de'),
-				InlineKeyboardButton('🇮🇹', callback_data = 'ru-it')],
+	keyboard = [[InlineKeyboardButton('🇬🇧󠁢󠁥󠁮󠁧󠁿', callback_data='en'),
+				InlineKeyboardButton('🇩🇪', callback_data='de'),
+				InlineKeyboardButton('🇮🇹', callback_data='it')],
 
-				[InlineKeyboardButton('🇪🇸', callback_data = 'ru-es'),
-				InlineKeyboardButton('🇫🇷', callback_data = 'ru-fr'),
-				InlineKeyboardButton('🇵🇱', callback_data = 'ru-pl')],
+				[InlineKeyboardButton('🇪🇸', callback_data='es'),
+				InlineKeyboardButton('🇫🇷', callback_data='fr'),
+				InlineKeyboardButton('🇵🇱', callback_data='pl')],
 
-				[InlineKeyboardButton('➕', callback_data = 'next')]
+				[InlineKeyboardButton('➕', callback_data='next')]
 	]
 	return InlineKeyboardMarkup(keyboard)
 
 # Второй ряд
 def lang_two():
-	keyboard = [[InlineKeyboardButton('🇵🇹', callback_data = 'ru-pt'),
-				InlineKeyboardButton('🇷🇸', callback_data = 'ru-sr'),
-				InlineKeyboardButton('🇨🇿', callback_data = 'ru-cs')],
+	keyboard = [[InlineKeyboardButton('🇵🇹', callback_data='pt'),
+				InlineKeyboardButton('🇷🇸', callback_data='sr'),
+				InlineKeyboardButton('🇨🇿', callback_data='cs')],
 
-				[InlineKeyboardButton('🇨🇳', callback_data = 'ru-zh'),
-				InlineKeyboardButton('🇺🇦', callback_data = 'ru-uk'),
-				InlineKeyboardButton('🇦🇪', callback_data = 'ru-ar')],
+				[InlineKeyboardButton('🇨🇳', callback_data='zh'),
+				InlineKeyboardButton('🇺🇦', callback_data='uk'),
+				InlineKeyboardButton('🇦🇪', callback_data='ar')],
 
-				[InlineKeyboardButton('➖', callback_data = 'back')]
+				[InlineKeyboardButton('➖', callback_data='back')]
 	]
 	return InlineKeyboardMarkup(keyboard)
 
 # Фукция обработчик команд от кнопок
-def lang_callback(bot, update):
+def lang_callback(update, bot):
 	query = update.callback_query
-	data = query.data
-	chat_id = update.effective_message.chat_id
-	current_text = update.effective_message.text
+	target_language = query.data
+	texts = [update.effective_message.text]
 
-	lange = 'lang={}&'.format(data)
+	if not target_language in ['next', 'back']:
+		body = {
+			"targetLanguageCode": target_language,
+			"texts": texts,
+			"folderId": folder_id,
+		}
 
-	if data == lange[5:10]:
-		url = yandex + lange + key + '&text={}'.format(current_text)
-		r = requests.get(url).json()
-		ans = r['text']
-		for i in ans:
-			return i
+		headers = {
+			"Content-Type": "application/json",
+			"Authorization": "Bearer {0}".format(IAM_TOKEN)
+		}
+
+		response = requests.post(
+			yandex,
+			json=body,
+			headers=headers
+		)
+
+		text = json.loads(response.text)['translations'][0]['text']
+
 		query.edit_message_text(
-			text = i,
-			parse_mode = ParseMode.MARKDOWN
-			)
-		bot.send_message(
-			chat_id = chat_id,
+			text=text,
+			parse_mode=ParseMode.MARKDOWN
+		)
+
+	elif target_language == 'next':
+		query.edit_message_text(
+			text=texts[0],
+			reply_markup=lang_two(),
 			)
 
-	elif data == 'next':
+	elif target_language == 'back':
 		query.edit_message_text(
-			text = current_text,
-			reply_markup = lang_two(),
-			)
-
-	elif data == 'back':
-		query.edit_message_text(
-			text = current_text,
-			reply_markup = lang(),
+			text=texts[0],
+			reply_markup=lang(),
 			)
 
 # Функция вывода, результат работы остальных функций
 def main():
-	bot = Bot(token = token)
-	updater = Updater(bot = bot)
-	update = Update
+	bot = Bot(token=token)
+	updater = Updater(bot=bot)
 
 	# Встроенные обработчки запросов от телеграмм
 	command_handler = CommandHandler('start', command)
-	help_command_handler = CommandHandler('🆘🤯⁉️', help_command)
+	help_command_handler = CommandHandler('info', help_command)
 	txt_handler = MessageHandler(Filters.text, txt)
-	buttons_handler = CallbackQueryHandler(callback = lang_callback)
+	buttons_handler = CallbackQueryHandler(callback=lang_callback)
 
 	# Менеджеры по выводу встроенных функций от телеграмм
 	updater.dispatcher.add_handler(command_handler)
+	updater.dispatcher.add_handler(help_command_handler)
 	updater.dispatcher.add_handler(txt_handler)
 	updater.dispatcher.add_handler(buttons_handler)
-	updater.dispatcher.add_handler(help_command_handler)
 
 	# Команда запуска скрпта и обработки его до конца всего цикла
 	updater.start_polling()
@@ -123,6 +126,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-
-
-
